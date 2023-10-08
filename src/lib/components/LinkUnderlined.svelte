0a88d5269fc73b9db01d8deffbfd854e3c1dc480
allgeo <allgeocode@gmail.com>
2023-10-08 14:22:49 -0400

    updated about us

--- src/lib/components/LinkUnderlined.svelte
+++ src/lib/components/LinkUnderlined.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <a href={href} class="text-sm transition-all duration-300 ease-in-out group ">
-  <span class="bg-gradient-to-r from-secondary to-lime-500 bg-[length:0%_1px] bg-left-bottom bg-no-repeat transition-all duration-500 ease-out group-hover:bg-[length:100%_1px]">
+  <span class="rounded bg-slate-100 w-fit py-1 px-2 bg-gradient-to-r from-secondary to-lime-500 bg-[length:0%_1px] bg-left-bottom bg-no-repeat transition-all duration-500 ease-out group-hover:bg-[length:100%_1px]">
     {text}
   </span>
 </a>
