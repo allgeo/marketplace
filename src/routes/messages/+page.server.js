acfc0e95e8c53a38f0e25cff7ac286f871d7530a
SEricL <eric_lan_2001@hotmail.com>
2023-10-07 23:03:36 -0400

    Initial DMS

--- src/routes/messages/+page.server.js
+++ src/routes/messages/+page.server.js
@@ -3,18 +3,31 @@ import { error } from "@sveltejs/kit";
 export async function load(event){
     let id = event.locals.session.user.id;
 
-    //Get rows, find one with provided id
-    const { data } = await event.locals.sb.from("Messages").select().or(`receiver_uid.eq.${id},sender_uid.eq.${id}`);
-    //console.log(id);
-    //console.log(data);
-    
+    //Get rows, find one with provided id .or(`receiver_uid.eq.${id}`)
+    //const { data, err } = await event.locals.sb.from("Messages").select(`id, created_at, message, receiver_uid, sender_uid, Users:receiver_uid(name), Users:sender_uid(name)`);
+    let { data, err} = await event.locals.sb.rpc("get_other_name", {my_uid:id});
+
     //No results, return 404
     if(data == null){
-        throw error(404, 'Forum not found');
+        throw error(404, 'Messages not found');
     }
     if(data.length <= 0){
-        throw error(404, 'Forum not found');
+        throw error(404, 'Messages not found');
     }
+
+    //ouid = uid of other person (not current user)
+    data.forEach(function(value, index){
+        value["ouid"] = id === value["receiver_uid"] ? value["sender_uid"] : value["receiver_uid"]
+    });
+
+    //Save most recent messages only
+    data.reverse();
+    data = data.filter((value, index, array) => index === array.findIndex((t) => (
+        t.ouid === value.ouid
+    )));
+
+    //DELETE or COMMENT in build
+    console.log("Sent:", data);
 
     return{
         messages:data,
